4df3849ac7bc121ad40f0f2f80eb2c60780dbf53
RodrigoBS98 <rodrigoberrios1998@gmail.com>
2020-04-23 16:22:43 -0500

    SEO -added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {SeoService} from 'src/app/seo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ieee-app';
+export class AppComponent implements OnInit {
+  
+  constructor(private title:Title, private seo:SeoService){}
+
+  ngOnInit(){
+    let t:string="IEEE Computer Society UTP";
+    this.title.setTitle(t);
+
+    this.seo.generateTags({
+      title:"IEEE Computer Society UTP",
+      description:"Somos una organizión dedicada a difundir conocimiento entre sus miembros",
+      slug:""
+    })
+
+  }
+
 }
